a58e89ddd33ba48394b03fcad1e830185bfa3249
vkshay3400 <akshayp2208@gmail.com>
2020-04-30 11:20:33 +0530

    Refactor: Greeting controller

--- src/main/java/com/bridgelabz/greetingapp/controller/GreetingController.java
+++ src/main/java/com/bridgelabz/greetingapp/controller/GreetingController.java
@@ -1,33 +1,18 @@
 package com.bridgelabz.greetingapp.controller;
 
-import com.bridgelabz.greetingapp.model.User;
+import com.bridgelabz.greetingapp.model.Greeting;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 @RestController
-@RequestMapping("/hello")
+@RequestMapping("/greetingController")
 public class GreetingController {
-    @RequestMapping(value = {"", "/", "/home"})
-    public String sayHello() {
-        return "Hello World!";
-    }
+    private static final String template ="Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping(value = {"/query"}, method = RequestMethod.GET)
-    public String sayHello(@RequestParam(value = "name") String name) {
-        return "Hello World " + name + "!";
-    }
-
-    @GetMapping("/param/{name}")
-    public String sayHelloParam(@PathVariable String name) {
-        return "Hello World " + name + "!";
-    }
-
-    @PostMapping("/post")
-    public String sayHello(@RequestBody User user) {
-        return "Hello " + user.getFirstName() + " " + user.getLastName() + "!";
-    }
-
-    @PutMapping("/put/{firstName}")
-    public String sayHello(@PathVariable String firstName, @RequestParam(value = "lastName") String lastName) {
-        return "Hello " + firstName + " " + lastName + "!";
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name){
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 }
