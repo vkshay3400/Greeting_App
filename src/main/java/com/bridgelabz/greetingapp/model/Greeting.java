cd196ebf1312c27ae1a384bbd50b065eee717a48
vkshay3400 <akshayp2208@gmail.com>
2020-04-30 11:21:05 +0530

    Added: Greeting model

---
+++ src/main/java/com/bridgelabz/greetingapp/model/Greeting.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.greetingapp.model;
+
+public class Greeting {
+    private final long incrementAndGet;
+    private final String format;
+
+    public Greeting(long incrementAndGet, String format) {
+        this.incrementAndGet = incrementAndGet;
+        this.format = format;
+    }
+
+    public long getIncrementAndGet() {
+        return incrementAndGet;
+    }
+
+    public String getFormat() {
+        return format;
+    }
+}
