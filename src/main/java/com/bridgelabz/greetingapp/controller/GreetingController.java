28b76e16ac789bd81bb02c1f887d583e377bf1ae
vkshay3400 <akshayp2208@gmail.com>
2020-05-01 15:26:13 +0530

    Modified: Changed request mapping and added message for hello world

--- src/main/java/com/bridgelabz/greetingapp/controller/GreetingController.java
+++ src/main/java/com/bridgelabz/greetingapp/controller/GreetingController.java
@@ -2,16 +2,20 @@ package com.bridgelabz.greetingapp.controller;
 
 import com.bridgelabz.greetingapp.model.Greeting;
 import com.bridgelabz.greetingapp.model.User;
+import com.bridgelabz.greetingapp.service.IGreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
-@RequestMapping("/hello")
+@RequestMapping("/greetingapp")
 public class GreetingController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
+    @Autowired
+    IGreetingService service;
 
     @GetMapping("/greetings")
     public Greeting greeting(long l, @RequestParam(value = "name", defaultValue = "World") String name) {
@@ -36,5 +40,10 @@ public class GreetingController {
     @PutMapping("/put/{firstName}")
     public Greeting greetingPut(@PathVariable String firstName, @RequestParam(value = "lastName") String lastName) {
         return new Greeting(counter.incrementAndGet(), String.format(template, firstName + " " + lastName));
+    }
+
+    @RequestMapping("/message")
+    public String getMessage(){
+        return service.getGreeting();
     }
 }
